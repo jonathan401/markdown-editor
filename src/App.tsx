2cb2bd5124fc1f50d66a2a1ad0401be57b37785e
jonathan401 <jonathangomina5049@gmail.com>
2022-11-09 01:18:52 +0100

    minor adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Editor from "./Editor";
 const App: FC = () => {
   return (
     <div>
-      App
       <Editor />
     </div>
   );
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
+import "./editor.css";
+
 // styles
 // import style from "./markdown-styles.module.css";
 
 const Editor = () => {
-  const [text, setText] = useState("");
+  const [editorContent, seteditorContent] = useState("");
   const [selectedTab, setSelectedTab] = useState<"write" | "preview">("write");
+
   return (
-    <div>
-      <div>
-        <button onClick={() => setSelectedTab("write")}>Write</button>
-        <button onClick={() => setSelectedTab("preview")}>Preview</button>
+    <div className="markdown-wrap">
+      <div className="btn-wrap">
+        <button
+          className={selectedTab === "write" ? "btn active" : "btn"}
+          onClick={() => setSelectedTab("write")}
+        >
+          Write
+        </button>
+        <button
+          className={selectedTab === "preview" ? "btn active" : "btn"}
+          onClick={() => setSelectedTab("preview")}
+        >
+          Preview
+        </button>
       </div>
       {selectedTab === "write" ? (
-        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+        <textarea
+          className="editor"
+          placeholder="Tell your story..."
+          value={editorContent}
+          onChange={(e) => seteditorContent(e.target.value)}
+        />
       ) : (
-        <ReactMarkdown>{text}</ReactMarkdown>
+        <ReactMarkdown className="preview">
+          {editorContent ? editorContent : "Nothing to see here 🌵"}
+        </ReactMarkdown>
       )}
     </div>
   );
