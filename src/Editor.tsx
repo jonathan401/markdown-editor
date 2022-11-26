c229f4d794cd40fb9f1d0423b3eff8e1a89013f9
jonathan401 <jonathangomina5049@gmail.com>
2022-11-26 23:23:28 +0100

    add more button functionality

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import { AiOutlineBold, AiOutlineItalic, AiOutlineEye } from "react-icons/ai";
+import { HiOutlineCode } from "react-icons/hi";
+import { BsLink45Deg, BsBlockquoteLeft } from "react-icons/bs";
+import {
+  MdOutlineFormatListBulleted,
+  MdOutlineFormatListNumbered,
+} from "react-icons/md";
+import { BiPencil } from "react-icons/bi";
+// import { materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 import "./editor.css";
-
-// styles
-// import style from "./markdown-styles.module.css";
 
 const Editor = () => {
   const [editorContent, seteditorContent] = useState("");
   const [selectedTab, setSelectedTab] = useState<"write" | "preview">("write");
+
+  const handleKey = (e: any) => {
+    if (e.code === "Tab") {
+      e.preventDefault();
+      seteditorContent((prevState) => prevState + "\t");
+    }
+  };
 
   return (
     <div className="markdown-wrap">
@@ -17,17 +31,76 @@ const Editor = () => {
           className={selectedTab === "write" ? "btn active" : "btn"}
           onClick={() => setSelectedTab("write")}
         >
-          Write
+          <BiPencil />
+          <span>Write</span>
         </button>
         <button
           className={selectedTab === "preview" ? "btn active" : "btn"}
           onClick={() => setSelectedTab("preview")}
         >
-          Preview
+          <AiOutlineEye />
+          <span>Preview</span>
         </button>
+        {selectedTab === "write" ? (
+          <div className="controls">
+            <button
+              onClick={() =>
+                seteditorContent((prevState) => prevState + "****")
+              }
+              className="btn"
+            >
+              <AiOutlineBold />
+            </button>
+            <button
+              onClick={() => seteditorContent((prevState) => prevState + "**")}
+              className="btn"
+            >
+              <AiOutlineItalic />
+            </button>
+            <button
+              onClick={() => seteditorContent((prevState) => prevState + "\n-")}
+              className="btn"
+            >
+              <MdOutlineFormatListBulleted />
+            </button>
+            <button
+              onClick={() =>
+                seteditorContent((prevState) => prevState + "\n1.")
+              }
+              className="btn"
+            >
+              <MdOutlineFormatListNumbered />
+            </button>
+            <button
+              onClick={() =>
+                seteditorContent((prevState) => prevState + "\n```\n```")
+              }
+              className="btn"
+            >
+              <HiOutlineCode />
+            </button>
+            <button
+              onClick={() =>
+                seteditorContent((prevState) => prevState + "[Text](Link)")
+              }
+              className="btn"
+            >
+              <BsLink45Deg />
+            </button>
+            <button
+              onClick={() => seteditorContent((prevState) => prevState + "\n>")}
+              className="btn"
+            >
+              <BsBlockquoteLeft />
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
       {selectedTab === "write" ? (
         <textarea
+          onKeyDown={handleKey}
           className="editor"
           placeholder="Tell your story..."
           value={editorContent}
